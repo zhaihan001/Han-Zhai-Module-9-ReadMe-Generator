9fe78d1c58e0f8374de1d6f62e9834cf7ae1f8b0
Han Zhai <zhaihan001@gmail.com>
2022-08-21 01:00:15 -0700

    application functional

--- Develop/README.md
+++ Develop/README.md
@@ -1,8 +1,8 @@
-# ge
+# README File Generator
 
 ## Description
 
-teh
+I want to build an application that generates professional README file with given information. This is a good opportunity to practice how node js work. I also learned how to use npm, inquirer, template literals, and fs.writefiles.
 
 ## Table of Contents
 
@@ -16,34 +16,45 @@ teh
 
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+ISC License
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
+        Copyright (c) 
+        
+        Permission to use, copy, modify, and/or distribute this software for any
+        purpose with or without fee is hereby granted, provided that the above
+        copyright notice and this permission notice appear in all copies.
+        
+        THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+        REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+        AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+        INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+        LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+        OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+        PERFORMANCE OF THIS SOFTWARE.
 
 ## Installation Instruction
 
-ryhj
+The application requires to install node modules inquirer. Run command "npm init -y" to install npm packages. Then run "npm i inquirer@8.2.4" to install inquier module with version 8.2.4
 
 ## Usage
 
-h
+Enter the answers in the prompt. Then a README file will be created
 
 ## How to Contribute
 
-ryjn
+The application is accessible from my Github profile
 
 ## License
 
-MIT
+ISC
 
 ## Tests
 
-hrsye
+Make sure the file is generated and no error message shows in terminal
 
 ## Questions
 
-My Github profile: https://github.com/rtewshj
+My Github profile: https://github.com/zhaihan001
 
-Please feel free reach me out with additional questions.
-My Email Address is: 
+Please feel free reach me with additional questions.
+My Email Address is: zhaihan001@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+var badge;
 // Create an array of questions for user input
 const questions = [
     {
@@ -52,6 +52,50 @@ const questions = [
     },
 ];
 
+function licenseText (license){
+    if (license == "MIT"){
+        badge = `MIT License
+
+        Copyright (c)
+        
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.`
+    }
+    else {
+        badge =`ISC License
+
+        Copyright (c) 
+        
+        Permission to use, copy, modify, and/or distribute this software for any
+        purpose with or without fee is hereby granted, provided that the above
+        copyright notice and this permission notice appear in all copies.
+        
+        THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+        REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+        AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+        INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+        LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+        OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+        PERFORMANCE OF THIS SOFTWARE.`
+    }
+    return badge;
+}
+
 const generateFile = ({title, description, installation, usage, contribution, test, license, username, email })=>
 `# ${title}
 
@@ -71,10 +115,7 @@ ${description}
 
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
+${licenseText()}
 
 ## Installation Instruction
 
@@ -100,7 +141,7 @@ ${test}
 
 My Github profile: https://github.com/${username}
 
-Please feel free reach me out with additional questions.
+Please feel free reach me with additional questions.
 My Email Address is: ${email}`
 
 
